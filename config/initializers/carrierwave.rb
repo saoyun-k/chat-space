03b450786d744f79bfbaa60c8724c27fb9359d3f
saoyun-k <12hiroki30@gmeil.com>
2020-01-24 00:06:13 +0900

    chat-space-deploy-7

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,5 +13,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'saoyundeployimage'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/saoyundeployimage'
+  config.asset_host = 'https://saoyundeployimage.s3-ap-northeast-1.amazonaws.com'
 end
